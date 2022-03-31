1c7d96e3c8b7d415d5df59ef19f98fadb403efd2
Dmitriy <d_antonov@list.ru>
2022-03-31 20:57:36 +0300

    fix author

--- api/resources/author.py
+++ api/resources/author.py
@@ -15,14 +15,16 @@ class AuthorResource(Resource):
     @auth.login_required
     def put(self, author_id):
         parser = reqparse.RequestParser()
-        parser.add_argument("name", required=True)
-        parser.add_argument("surname", required=True)
+        parser.add_argument("name")
+        parser.add_argument("surname")
         author_data = parser.parse_args()
         author = AuthorModel.query.get(author_id)
         if author is None:
             return {"Error": f"Author id={author_id} not found"}, 404
-        author.name = author_data["name"]
-        author.surname = author_data["surname"]
+
+        for key, value in author_data.items():
+            setattr(author, key, value)
+
         db.session.commit()
         return author_schema.dump(author), 200
 
